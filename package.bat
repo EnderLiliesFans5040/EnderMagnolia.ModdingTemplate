927cd86e93bcbcdec1a50813a00bafa6cba613aa
TipherethA <TA@noexist.io>
2024-03-28 11:39:19 +0800

    fixed debug menu invoker

--- package.bat
+++ package.bat
@@ -1,6 +1,6 @@
 set UE5PATH=D:\prog\Epic Games\UE5\UE_5.3
 set MODNAME=DebugMenu
-
+set GAMEPATH=D:\EnderMagnoliaMod\ENDER MAGNOLIA\EnderMagnolia
 set PROJPATH=%~dp0
 set PROJPATH=%PROJPATH:~0,-1%
 for %%I in (.) do set PROJNAME=%%~nxI
@@ -12,5 +12,14 @@ call "%UE5PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun^
  -prereqs -nodebuginfo -manifests -targetplatform=Win64 -clientconfig=Development -utf8output -iostore^
  -cook -stage -pak -package^
  -ddc=InstalledDerivedDataBackendGraph
+
+if exist "%PROJPATH%\%MODNAME%\" RD /S /Q "%PROJPATH%\%MODNAME%"
+MKDIR "%PROJPATH%\%MODNAME%"
+XCOPY "%PROJPATH%\Saved\StagedBuilds\Windows\%PROJNAME%\Content\Paks\*chunk1*" "%PROJPATH%\%MODNAME%\."
+For /R "%PROJPATH%\%MODNAME%\" %%# in ("*.*") Do (
+  ren "%%#" "%MODNAME%_0_P%%~x#"
+)
+if exist "%GAMEPATH%\Content\Paks\%MODNAME%\" RD /S /Q "%GAMEPATH%\Content\Paks\%MODNAME%%"
+XCOPY /Y /C /I /E "%PROJPATH%\%MODNAME%" "%GAMEPATH%\Content\Paks\%MODNAME%"
 
 pause
